853f0af48579aaea9799f601a14b622d91bed14f
Kasia <katarzyna.jankowska000728@gmail.com>
2023-03-13 16:30:55 +0100

    Add perecisionfunctionality

--- lab3/main.py
+++ lab3/main.py
@@ -10,6 +10,14 @@ def my_printf(format_string,param):
     for i in range(len(words)):
         if words[i] == "k#":
             words[i] = param
+        elif words[i].startswith("#.") and words[i].endswith("k"):
+            num = words[i][2:-1]
+            if num.isnumeric():
+                if int(num) < len(param):
+                    str = param[:int(num)]
+                    words[i] = str
+                else:
+                    words[i] = param
     
     print(" ".join(words))
 
