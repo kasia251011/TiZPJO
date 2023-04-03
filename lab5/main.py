b23c1b7939c47f948daf53fa93a7173396f0ea8c
kasia251011 <katarzyna.jankowska000728@gmail.com>
2023-04-03 17:00:00 +0200

    Add substricing digit

--- lab5/main.py
+++ lab5/main.py
@@ -1,8 +1,26 @@
 #!/usr/bin/env python3
 
 import sys
+import re
 
 def my_printf(format_string,param):
+    list1 = list(param)
+    for i in range(len(list1)):
+        if list1[i] == '0':
+            list1[i] = '9'
+        else:
+            list1[i] = str(int(list1[i]) - 1)
+    param = ''.join(list1)
+
+    x = re.search("#\d+g", format_string)
+    if x:    
+        format = x.group()
+        num = format[1:-1]
+        if num.isnumeric():
+            s = param.rjust(int(num), ' ')
+            x = re.sub("#\d+g", s, format_string)
+            print(x)
+            return
     print(re.sub("#g", param, format_string))
 
 data=sys.stdin.readlines()
