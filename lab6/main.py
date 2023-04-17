b2161b90ce5b61ab790952399356df7764fc2292
kasia251011 <katarzyna.jankowska000728@gmail.com>
2023-04-17 16:40:46 +0200

    Add replace format

--- lab6/main.py
+++ lab6/main.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python3
 
 import sys
+import re
 
 def my_printf(format_string,param):
-    
+    x = re.search("#\.\d+g", format_string)
+    param = str(param)
+    if x:    
+        format = x.group()
+        num = format[2:-1]
+        s = param.rjust(int(num), ' ')
+        x = re.sub("#\.\d+g", s, format_string)
+        print(x)
+        return
+    print(format_string)
 
 data=sys.stdin.readlines()
 
