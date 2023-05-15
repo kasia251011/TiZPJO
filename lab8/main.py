97e9782b48c970d5d8aa11e5b1978970e0a89663
kasia251011 <katarzyna.jankowska000728@gmail.com>
2023-05-15 17:46:57 +0200

    Implement basic functionality

--- lab8/main.py
+++ lab8/main.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python3
 
 import sys
+import re
 
 def my_printf(format_string,param):
-
+    param = str(hex(int(10)))[2:]
+    param = param.replace("a","g")
+    param = param.replace("b","h")
+    param = param.replace("c","i")
+    param = param.replace("d","j")
+    param = param.replace("e","k")
+    param = param.replace("f","l")
+    x = re.sub("#j", param, format_string)
+    print(x)
 
 data=sys.stdin.readlines()
 
