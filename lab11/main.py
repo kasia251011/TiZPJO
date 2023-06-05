404fda0c2bf6183c36e651e7417831e96de4b3f2
kasia251011 <katarzyna.jankowska000728@gmail.com>
2023-06-05 17:51:32 +0200

    Implement replacment

--- lab11/main.py
+++ lab11/main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import sys
+import re
 
 def convert(number):
     res = bin(number)[2:]
@@ -22,18 +23,8 @@ def convert(number):
     return res
 
 def my_printf(format_string,param):
-    #print(format_string)
-    shouldDo=True
-    for idx in range(0,len(format_string)):
-        if shouldDo:
-            if format_string[idx] == '#' and format_string[idx+1] == 'k':
-                print(param,end="")
-                shouldDo=False
-            else:
-                print(format_string[idx],end="")
-        else:
-            shouldDo=True
-    print("")
+    res = convert(int(param))
+    print(re.sub("#b", res, format_string))
 
 data=sys.stdin.readlines()
 
